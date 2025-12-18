d7df8e732e81282cc3f42953e4fd7270740678cf
Tr1cky3 <anshorullah3@gmail.com>
2025-12-18 16:21:44 +0700

    small changes to end this agaony

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const BtnGoal = document.getElementById('BtnGoal');
 const sidebar2 = document.querySelector('.sidebar2');
+const sidebar1 = document.querySelector('.sidebar1');
+const BtnPro = document.getElementById('BtnPro');
+const BtnCnt = document.getElementById('BtnCnt');
 
 BtnGoal.addEventListener('click', function(){
     if(sidebar2.style.display === 'none'){
@@ -7,4 +10,14 @@ BtnGoal.addEventListener('click', function(){
     } else{
         sidebar2.style.display = 'none';
     }
+})
+BtnPro.addEventListener('click', function(){
+    if(sidebar1.style.display === 'none'){
+        sidebar1.style.display = 'block';
+    } else{
+        sidebar1.style.display = 'none';
+    }
+})
+BtnCnt.addEventListener('click', function(){
+    window.location.href = "https://linktr.ee/mrtricky";
 })
